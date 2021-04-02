b2068dae1dc9fdea30e86584d605ce3fc1528762
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-04-02 15:19:18 +0200

    atatus added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,7 +18,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
-
+atatus.config('b402e4864af94b3980806c1367f7b389').install();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
